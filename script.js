c614858dc219acf66c0b1257a72c451c9aee87e6
divijkhaitan <divijkhaitan@gmail.com>
2022-08-26 15:28:02 +0530

    Add read status and toggle button

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let container=document.querySelector('.gridcontainer');
-function book(title, author, pages, status){
+function book(title, author, pages, status, read){
     this.title=title
     this.author=author
     this.pages=pages
     this.status=status
+    this.read=read
 }
 book.prototype.availability=function()
 {
@@ -17,39 +18,43 @@ book.prototype.availability=function()
     }
 }
 
+book.prototype.haveRead=function()
+{
+    if(this.read)
+    {
+        return "Completed"
+    }
+    else
+    {
+        return "Unread"
+    }
+}
+book.prototype.toggleRead=function()
+{
+    this.read= !(this.read)
+}
+
+
 book.prototype.info= function(){
     console.log(`Name: ${this.title}\nAuthor: ${this.author}\nPages: ${this.pages}\nAvailability: ${this.availability}`)
 }
 
-const percy1=new book("Lightning Thief", "Rick Riordan", 254, true)
-const clues39=new book ("One False Note", "Gordan Korman",198, false)
-const artemis=new book ("Aretmis Fowl", "Eoin Colfer", 225, true)
-const tweak=new book ("Tweak", "Nic Sheff", 322, false)
-const alex= new book ("Skeleton Key", "Anthony Horowitz", 313, false)
-const heroes=new book ("House of Hades", "Rick Riordan", 455, false)
-const dragonball=new book("Dragon Ball Volume 1", "Akira Toriyama", 212, true)
+const percy1=new book("Lightning Thief", "Rick Riordan", 254, true, true)
+const clues39=new book ("One False Note", "Gordan Korman",198, false, true)
+const artemis=new book ("Aretmis Fowl", "Eoin Colfer", 225, true, true)
+const tweak=new book ("Tweak", "Nic Sheff", 322, false, true)
+const alex= new book ("Skeleton Key", "Anthony Horowitz", 313, false, false)
+const heroes=new book ("House of Hades", "Rick Riordan", 455, false, false)
+const dragonball=new book("Dragon Ball Volume 1", "Akira Toriyama", 212, true, false)
 
-let catalog=[percy1, clues39, artemis, tweak, alex, heroes, dragonball]
+let initcatalog=[percy1, clues39, artemis, tweak, alex, heroes, dragonball]
 let divcat=[];
-function populateSite(){
-    let currentcard;
-    let nodes;
-    for (let i=0; i<catalog.length; i++)
-    {
-        currentcard=document.querySelector(`[data='${i}']`)
-        nodes=currentcard.children;
-        nodes[0].textContent=`Name: ${catalog[i].title}`
-        nodes[1].textContent=`Author: ${catalog[i].author}`
-        nodes[2].textContent=`Pages: ${catalog[i].pages}`
-        nodes[3].textContent=`Availability: ${catalog[i].availability()}`
-    }
-}
-//populateSite()
+let catalog=[]
 
 function populateSiten(){
-    for (let i=0; i<catalog.length; i++)
+    for (let i=0; i<initcatalog.length; i++)
     {
-        addbook(catalog[i])
+        addbook(initcatalog[i])
     }
 }
 populateSiten()
@@ -63,6 +68,7 @@ function addbook(newbook)
     let currentPages;
     let currentStatus;
     let currentButtons;
+    let currentRead;    
         currentcard=document.createElement('div')//currentcard=document.querySelector(`[data='${i}']`)
         currentcard.classList.add('cards')
         currentcard.setAttribute('data',`${divcat.length}`)
@@ -83,23 +89,35 @@ function addbook(newbook)
         currentStatus.classList.add('status')
         currentStatus.textContent=`Availability: ${newbook.availability()}`
         
+        currentRead=document.createElement('div')
+        currentRead.classList.add('read')
+        currentRead.textContent=`Completion: ${newbook.haveRead()}`
+        
         currentButtons=document.createElement('div')
         currentButtons.classList.add('buttonbar')
+        
         remove=document.createElement('button')
         remove.classList.add('button')
         remove.addEventListener('click', clicked)
         remove.textContent="Remove"
         currentButtons.appendChild(remove)
-        
+
+        readToggle=document.createElement('button')
+        readToggle.classList.add('button')
+        readToggle.addEventListener('click', toggle)
+        readToggle.textContent="Toggle Completion"
+        currentButtons.appendChild(readToggle)
 
         currentcard.appendChild(currentTitle)
         currentcard.appendChild(currentAuth)
         currentcard.appendChild(currentPages)
         currentcard.appendChild(currentStatus)
+        currentcard.appendChild(currentRead)
         currentcard.appendChild(currentButtons)
 
         container.appendChild(currentcard)
         divcat.push(currentcard)
+        catalog.push(newbook)
 }
 
 function clicked(e)
@@ -117,4 +135,13 @@ function clicked(e)
         console.log(divcat[i].getAttribute('data'))
         divcat[i].setAttribute('data',`${parseInt(divcat[i].getAttribute('data')) - 1}`)
     }
+}
+
+function toggle(e)
+{
+    let currentcard=e.target.parentNode.parentNode;
+    index=parseInt(currentcard.getAttribute('data'))
+    catalog[index].toggleRead();
+    divcat[index].children[4].textContent=`Completion: ${catalog[index].haveRead()}`;
+
 }
